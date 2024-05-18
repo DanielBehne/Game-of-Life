eb5c768c3419194b7d286b994ae73caf1c2c3346
Daniel Behne <daniel.behne@mrjaffesclass.com>
2024-05-17 20:30:53 -0700

    added algroithms and noUser

--- vsLife/src/MainFrame.java
+++ vsLife/src/MainFrame.java
@@ -1,43 +1,48 @@
+import java.awt.Color;
+//import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-
+//import java.awt.Graphics2D;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import java.awt.event.*;
 
-import java.awt.BorderLayout;
-
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-public class MainFrame extends JPanel {
+public class MainFrame extends JPanel implements ActionListener {
     private final int CELL_SIZE = 25;
-    
+
     private int x = 0;
     private int y = 0;
-    
+
     private boolean[][] board = new boolean[32][32];
-    
-    JButton startButton;
-    
+
+    private static JButton startButton;
+
+    private static boolean gameOn = false;
 
     public MainFrame() {
-        startButton = new JButton();
-        startButton.setBounds(100,100,100,100);
-        
+        startButton = new JButton("Start");
+        startButton.setBounds(675, 25, 100, 50);
+        startButton.setPreferredSize(new Dimension(100, 75));
+        startButton.addActionListener(this);
+        startButton.setBackground(Color.lightGray);
+        startButton.setBorder(BorderFactory.createRaisedBevelBorder());
+        //add(startButton);
+
         for (int r = 0; r < 32; r++) {
             for (int c = 0; c < 32; c++) {
                 board[r][c] = false;
             }
         }
-        
+
         // Add a MouseListener to capture mouse clicks
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 // Update the coordinates where the mouse was clicked
-                x = e.getX()/CELL_SIZE;
-                y = e.getY()/CELL_SIZE;
+                x = e.getX() / CELL_SIZE;
+                y = e.getY() / CELL_SIZE;
                 if (board[x][y]) {
                     board[x][y] = false;
                 } else {
@@ -48,31 +53,17 @@ public class MainFrame extends JPanel {
             }
         });
 
-        this.add(startButton);
-
-    }
-    
-    public void init() {
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        setSize(500,500);
-        setVisible(true);
-        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     }
 
+    /************* PAINTING METHODS *************/
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //Graphics2D g2d = (Graphics2D) g;
         drawGrid(g);
-        
         drawSquare(g);
 
     }
 
-
-
-
-/*************PAINTING METHODS*************/    
     private void drawGrid(Graphics g) {
         for (int i = 0; i < 32; i++) {
             g.drawLine(i, (25 * i + 25), 800, (25 * i + 25));
@@ -81,29 +72,87 @@ public class MainFrame extends JPanel {
             g.drawLine((25 * i + 25), i, (25 * i + 25), 800);
         }
     }
-    
+
     private void drawSquare(Graphics g) {
         for (int r = 0; r < 32; r++) {
             for (int c = 0; c < 32; c++) {
                 if (board[r][c]) {
-                    g.fillRect(r*25, c*25, 25, 25);
+                    g.fillRect(r * 25, c * 25, 25, 25);
                 }
             }
         }
     }
 
+    /************* BUTTONS *************/
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startButton) {
+            startButton.setText("Stop");
+            gameOn = true;
+            flow(board);
+            while (gameOn) {
+                boolean[][] newBoard = flow(board);
+                board = newBoard;
+                repaint();
+                // try {
+                // Thread.sleep(250);
+                // repaint();
+                // }
+                // catch(InterruptedException er) {
+                // er.printStackTrace();
+                // }
+            }
+        }
+    }
 
-
-
-
-/*************MAIN METHOD*************/    
+    /************* MAIN METHOD *************/
     public static void main(String[] args) {
-        JFrame frame = new JFrame("example");
-        MainFrame mainFrame = new MainFrame();
-
+        JFrame frame = new JFrame("Game Of Life");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
+        MainFrame mainFrame = new MainFrame();
         frame.add(mainFrame);
+        mainFrame.add(startButton);
         frame.setVisible(true);
+    }
+
+    /************* GAME LOGIC *************/
+    public boolean[][] flow(boolean[][] arr) {
+        boolean[][] retArr = new boolean[arr.length][arr[0].length];
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                retArr[r][c] = isLive(arr, r, c);
+            }
+        }
+        return retArr;
+    }
+
+    public boolean isLive(boolean[][] arr, int row, int col) {
+        int count = 0;
+        for (int r = row - 1; row < row + 2; row++) {
+            for (int c = col - 1; col < col + 2; col++) {
+                if (r == row && c == col) {
+                    continue;
+                }
+                if (arr[r][c]) {
+                    count++;
+                }
+            }
+        }
+
+        if (arr[row][col]) {
+            if (count < 2 || count > 3) {
+                return false;
+            }
+            if (count == 2 || count == 3) {
+                return true;
+            }
+        }
+        if (!arr[row][col]) {
+            if (count == 3) {
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ vsLife/src/noUserInput.java
@@ -0,0 +1,131 @@
+// import java.awt.Color;
+//import java.awt.Font;
+import java.awt.Graphics;
+//import java.awt.Graphics2D;
+// import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+// import javax.swing.BorderFactory;
+// import javax.swing.JButton;
+// import java.awt.event.*;
+import javax.swing.Timer;
+
+public class noUserInput extends JPanel {
+    private Timer timer;
+    
+    //private final int CELL_SIZE = 25;
+
+    private int x = 0;
+    private int y = 0;
+
+    boolean[][] board = new boolean[32][32];
+
+
+    private static boolean gameOn = false;
+
+    public noUserInput() {
+
+        for (int r = 0; r < 32; r++) {
+            for (int c = 0; c < 32; c++) {
+                board[r][c] = false;
+            }
+        }
+        board[10][10] = true;
+        board[11][11] = true;
+        board[12][11] = true;
+        board[12][10] = true;
+        board[12][9] = true;
+
+        timer = new Timer(500, e -> {
+            flow();
+            repaint();
+        });
+        timer.start();
+
+    }
+
+    /************* PAINTING METHODS *************/
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawGrid(g);
+        drawSquare(g);
+
+    }
+
+    private void drawGrid(Graphics g) {
+        for (int i = 0; i < 32; i++) {
+            g.drawLine(i, (25 * i + 25), 800, (25 * i + 25));
+            g.drawLine((25 * i + 25), i, (25 * i + 25), 800);
+
+        }
+    }
+
+    private void drawSquare(Graphics g) {
+        for (int r = 0; r < 32; r++) {
+            for (int c = 0; c < 32; c++) {
+                if (board[r][c]) {
+                    g.fillRect(r * 25, c * 25, 25, 25);
+                }
+            }
+        }
+    }
+
+
+    /************* GAME LOGIC *************/
+    public void flow() {
+        boolean[][] retArr = new boolean[board.length][board[0].length];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                retArr[r][c] = isLive(board, r, c);
+            }
+        }
+        board = retArr;
+        //repaint();
+    }
+
+    public boolean isLive(boolean[][] arr, int row, int col) {
+        int count = 0;
+        for (int r = row - 1; row < row + 2; row++) {
+            for (int c = col - 1; col < col + 2; col++) {
+                if (arr[r][c]) {
+                    count++;
+                }
+            }
+        }
+
+        if (arr[row][col]) {
+            count--;
+            if (count < 2 || count > 3) {
+                return false;
+            }
+            if (count == 2 || count == 3) {
+                return true;
+            }
+        }
+        if (!arr[row][col]) {
+            if (count == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /************* MAIN METHOD *************/
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("noUserInput");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        noUserInput mainFrame = new noUserInput();
+        frame.add(mainFrame);
+        frame.setVisible(true);
+        // mainFrame.flow();
+        // mainFrame.repaint();
+        // try {
+        //     Thread.sleep(150);
+        //     String[] a = new String[1];
+        //     main(a);
+        // } catch (InterruptedException ex) {}
+    }
+}
